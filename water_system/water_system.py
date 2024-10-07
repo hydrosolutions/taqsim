65155e4d30f86c7fab9710a72aeea16a465515b1
Tobias Siegfried <siegfried@hydrosolutions.ch>
2024-10-07 20:29:58 +0200

    Code documentation added

--- water_system/water_system.py
+++ water_system/water_system.py
@@ -1,20 +1,63 @@
+"""
+This module defines the WaterSystem class, which is the main class for creating and managing
+a water system simulation. It uses NetworkX for graph representation and matplotlib for visualization.
+
+The WaterSystem class allows users to add nodes and edges to the system, run simulations,
+and visualize the results.
+"""
+
 import networkx as nx
 import matplotlib.pyplot as plt
 from .structure import SupplyNode, StorageNode, DemandNode
 
 class WaterSystem:
+    """
+    Represents a water system as a directed graph and provides methods for simulation and visualization.
+
+    Attributes:
+        graph (nx.DiGraph): A NetworkX directed graph representing the water system.
+        time_steps (int): The number of time steps in the most recent simulation.
+    """
+
     def __init__(self):
+        """
+        Initialize a new WaterSystem instance.
+        """
         self.graph = nx.DiGraph()
         self.time_steps = 0
 
     def add_node(self, node):
+        """
+        Add a node to the water system.
+
+        Args:
+            node (Node): The node to be added to the system.
+
+        This method adds the node to the graph and stores its type as an attribute.
+        """
         node_type = type(node).__name__
         self.graph.add_node(node.id, node=node, node_type=node_type)
 
     def add_edge(self, edge):
+        """
+        Add an edge to the water system.
+
+        Args:
+            edge (Edge): The edge to be added to the system.
+
+        This method adds the edge to the graph, connecting its source and target nodes.
+        """
         self.graph.add_edge(edge.source.id, edge.target.id, edge=edge)
 
     def simulate(self, time_steps):
+        """
+        Run a simulation of the water system for a specified number of time steps.
+
+        Args:
+            time_steps (int): The number of time steps to simulate.
+
+        This method updates each node and edge in the system for each time step.
+        """
         self.time_steps = time_steps
         for t in range(time_steps):
             for node_id in self.graph.nodes():
@@ -23,6 +66,12 @@ class WaterSystem:
                 edge_data['edge'].update(t)
 
     def visualize(self):
+        """
+        Visualize the water system using matplotlib.
+
+        This method creates a multipartite layout of the system, with nodes color-coded by type
+        and edges labeled with their final flow values.
+        """
         # Determine node layers for multipartite layout
         layers = {'SupplyNode': 0, 'StorageNode': 1, 'DemandNode': 2}
         colors = []
